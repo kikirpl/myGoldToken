ca582784a9ae8d09512883d39d076658cc7b5686
kikirpl <rizkyibrohim88@gmail.com>
2025-07-01 13:59:12 +0700

    tail

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const isGithubPages = process.env.NODE_ENV === "production";
-<<<<<<< HEAD
+
 const repo = "myGoldToken"; // Ganti dengan nama repo kamu
-=======
-const repo = "myGoldToken";
->>>>>>> gh-pages
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,10 +11,8 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
-<<<<<<< HEAD
-=======
+
   trailingSlash: true, // Tambahkan ini untuk GitHub Pages
->>>>>>> gh-pages
 };
 
 module.exports = nextConfig;
